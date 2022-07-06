ae3b49208bf4f7bbaf81f876ea1fd21688bf1e87
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-07-06 17:01:42 +0200

    update 404 page
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ export default function FourOhFour() {
     <div className="text-center">
       <img src="olifant.svg" className="w-full"/>
       <p className="text-xl">Ouch, we zijn in de diepe en grote jungle verdwaald.</p>
-      <button onClick={() => history.back()} className="border-[2px] mr-[40px] font-sans border-black p-2">Ga terug!</button>
+      <a href="/" className="border-[2px] my-[20px] font-sans border-black p-2">Ga terug!</a>
       <p className="text-lg">Deze pagina kon niet gevonden worden</p>
     </div>
   </>
