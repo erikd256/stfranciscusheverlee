1e84dd62e7bdd18646fec1d7341377c9e82f069d
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-07-06 16:58:42 +0200

    shrink header deps
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,41 +1,6 @@
 import React from "react";
-import Link from "next/link";
-import { Container } from "../util/container";
-import { useTheme } from ".";
-import { Icon } from "../util/icon";
-import Image from 'next/image'
 
 export const Header = ({ data }) => {
-  const theme = useTheme();
-
-  const headerColor = {
-    default:
-      "text-black dark:text-white from-gray-50 to-white dark:from-gray-700 dark:to-gray-800",
-    primary: {
-      blue: "text-white from-blue-300 to-blue-500",
-      teal: "text-white from-teal-400 to-teal-500",
-      green: "text-white from-green-400 to-green-500",
-      red: "text-white from-red-400 to-red-500",
-      pink: "text-white from-pink-400 to-pink-500",
-      purple: "text-white from-purple-400 to-purple-500",
-      orange: "text-white from-orange-400 to-orange-500",
-      yellow: "text-white from-yellow-400 to-yellow-500",
-    },
-  };
-
-  const headerColorCss = headerColor.primary[theme.color];
-
-  const activeItemClasses = {
-    blue: "border-b-3 border-blue-200 dark:border-blue-700",
-    teal: "border-b-3 border-teal-200 dark:border-teal-700",
-    green: "border-b-3 border-green-200 dark:border-green-700",
-    red: "border-b-3 border-red-300 dark:border-red-700",
-    pink: "border-b-3 border-pink-200 dark:border-pink-700",
-    purple: "border-b-3 border-purple-200 dark:border-purple-700",
-    orange: "border-b-3 border-orange-200 dark:border-orange-700",
-    yellow: "border-b-3 border-yellow-300 dark:border-yellow-600",
-  };
-
   // If we're on an admin path, other links should also link to their admin paths
   const [prefix, setPrefix] = React.useState("");
   const [windowUrl, setUrl] = React.useState("");
