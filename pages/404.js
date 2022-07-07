b817535583ceac2f619fc58c9823783c69bf0fcc
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-07-07 17:18:10 +0200

    update 404 page
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ export default function FourOhFour() {
   <>
     <title>Error 404</title>
     <div className="text-center">
-      <img src="olifant.svg" className="w-full"/>
+      <img src="/olifant.svg" className="w-full"/>
       <p className="text-xl">Ouch, we zijn in de diepe en grote jungle verdwaald.</p>
       <a href="/" className="border-[2px] my-[20px] font-sans border-black p-2">Ga terug!</a>
       <p className="text-lg">Deze pagina kon niet gevonden worden</p>
