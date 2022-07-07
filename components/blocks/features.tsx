994100df2e168b6b8d6ecf3d7cc250dca5eacca1
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-07-07 20:40:24 +0200

    update features.tsx
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- components/blocks/features.tsx
+++ components/blocks/features.tsx
@@ -42,7 +42,7 @@ export const Feature = ({ featuresColor, data, tinaField }) => {
 
 export const Features = ({ data, parentField }) => {
   return (
-    <Section>
+    <Section color={data.color}>
       <Container
         className={`flex flex-wrap gap-x-10 gap-y-8 text-left`}
         size="large"
