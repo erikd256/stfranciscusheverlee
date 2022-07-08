2afe6cb413fef5b66fa24ec162611f2f7cc2fbbc
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-07-08 20:10:28 +0200

    added footer social link
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { FaFacebookF, FaGithub, FaTwitter } from "react-icons/fa";
-import { AiFillInstagram } from "react-icons/ai";
+import { FaFacebookF, FaGithub, FaYoutube } from "react-icons/fa";
 import { Container } from "../../util/container";
 import { RawRenderer } from "./rawRenderer";
 import { useTheme } from "..";
@@ -16,8 +15,10 @@ export const Footer = ({ data, icon, rawData }) => {
         <div className="flex justify-between text-liturgischekleur gap-6 flex-wrap">
           <p>&copy; <a href="https://github.com/erikd256" target="_blank">erikd256</a> - {(new Date().getFullYear())}</p>
           <p className={``}><img className="" src="https://vercelbadge.vercel.app/api/erikd256/stfranciscusheverlee?style=for-the-badge"/></p>
-          <span><a className="inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.facebook.com/pg/Franciscusgemeenschapheverlee"><FaFacebookF className={`${socialIconClasses}` }/></a>
-          <a className="inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://github.com/erikd256/stfranciscusheverlee"><FaGithub className={`${socialIconClasses}` }/></a>
+          <span>
+          <a className="mx-[0px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.facebook.com/pg/Franciscusgemeenschapheverlee"><FaFacebookF className={`${socialIconClasses}` }/>  </a>  
+          <a className="mx-[4px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.youtube.com/channel/UC-7wzVLv2W3oUQtQcPpvCig"><FaYoutube className={`${socialIconClasses}` }/>  </a>
+          <a className="mx-[4px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://github.com/erikd256/stfranciscusheverlee"><FaGithub className={`${socialIconClasses}` }/>  </a>
           </span>
         </div>
       </Container>
