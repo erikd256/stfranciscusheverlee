8863409b05a76a75fc80ba2be5959d3030c51e72
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-07-09 17:59:33 +0200

    remove whitespace in footer
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -16,9 +16,9 @@ export const Footer = ({ data, icon, rawData }) => {
           <p>&copy; <a href="https://github.com/erikd256" target="_blank">erikd256</a> - {(new Date().getFullYear())}</p>
           <p className={``}><img className="" src="https://api.netlify.com/api/v1/badges/d36450a6-4bd9-44e7-a837-3a8417574065/deploy-status"/></p>
           <span>
-          <a className="mx-[0px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.facebook.com/pg/Franciscusgemeenschapheverlee"><FaFacebookF className={`${socialIconClasses}` }/>  </a>  
-          <a className="mx-[4px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.youtube.com/channel/UC-7wzVLv2W3oUQtQcPpvCig"><FaYoutube className={`${socialIconClasses}` }/>  </a>
-          <a className="mx-[4px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://github.com/erikd256/stfranciscusheverlee"><FaGithub className={`${socialIconClasses}` }/>  </a>
+          <a className="mx-[0px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.facebook.com/pg/Franciscusgemeenschapheverlee"><FaFacebookF className={`${socialIconClasses}` }/></a>  
+          <a className="mx-[4px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.youtube.com/channel/UC-7wzVLv2W3oUQtQcPpvCig"><FaYoutube className={`${socialIconClasses}` }/></a>
+          <a className="mx-[4px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://github.com/erikd256/stfranciscusheverlee"><FaGithub className={`${socialIconClasses}` }/></a>
           </span>
         </div>
       </Container>
