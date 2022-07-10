fe0dd8092babc8839f0f9bc38efcbd9af570fa35
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-07-10 11:35:49 +0200

    enable branch switcher
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -384,8 +384,7 @@ export const tinaConfig = defineConfig({
     /**
      * Enables experimental branch switcher
      */
-    cms.flags.set("branch-switcher", false);
-
+    cms.flags.set("branch-switcher", true);
     /**
      * When `tina-admin` is enabled, this plugin configures contextual editing for collections
      */
