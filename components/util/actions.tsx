76a19ba687b6c8bad5fff449ef80b7b4193da20d
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-07-27 12:19:32 +0200

    update actions.tsx
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- components/util/actions.tsx
+++ components/util/actions.tsx
@@ -56,7 +56,7 @@ export const Actions = ({
   };
 
   return (
-    <div className={`flex flex-wrap items-center gap-y-4 gap-x-6 ${className}`}>
+    <div className={`flex flex-wrap items-center max-w-5/6 gap-y-4 gap-x-6 ${className}`}>
       {actions &&
         actions.map(function (action, index) {
           let element = null;
@@ -65,11 +65,7 @@ export const Actions = ({
               <Link key={index} href={action.link ? action.link : "/"}>
                 <button
                   data-tinafield={`${parentField}.${index}`}
-                  className={`z-10 relative flex items-center px-7 py-3 font-semibold text-lg transition duration-150 ease-out  rounded transform focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 whitespace-nowrap ${
-                    parentColor === "primary"
-                      ? invertedButtonColorClasses[theme.color]
-                      : buttonColorClasses[theme.color]
-                  }`}
+                  className={`z-10 relative flex items-center px-7 py-3 font-semibold text-lg transition duration-150 ease-out  rounded transform focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 whitespace-nowrap bg-basiskleur text-liturgischekleur`}
                 >
                   {action.label}
                   {action.icon && (
@@ -86,11 +82,7 @@ export const Actions = ({
               <Link key={index} href={action.link ? action.link : "/"} passHref>
                 <a
                   data-tinafield={`${parentField}.${index}`}
-                  className={`group inline-flex items-center font-semibold text-lg transition duration-150 ease-out ${
-                    parentColor === "primary"
-                      ? `text-white  hover:text-gray-50`
-                      : linkButtonColorClasses[theme.color]
-                  }`}
+                  className={`group inline-flex items-center font-semibold text-lg transition duration-150 ease-out text-basiskleur`}
                   style={{
                     textShadow: `0 3px 7px rgba(var(--color-rgb-blue-400),0.2)`,
                   }}
