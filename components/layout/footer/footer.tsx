4ef7402810cd4ffa00f70dc83a9c615217d680c1
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-08-01 18:39:24 +0200

    update README.md and footer.tsx
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -11,7 +11,12 @@ export const Footer = ({ data, icon, rawData }) => {
       <Container className="relative" size="small">
         <div className="flex justify-between text-liturgischekleur gap-6 flex-wrap ">
           <p>&copy; <a href="https://github.com/erikd256" target="_blank">erikd256</a> - {(new Date().getFullYear())}</p>
-          <p className={``}><img className="" src="https://vercel-badge-taupe.vercel.app/api/erikd256/stfranciscusheverlee?style=flat"/></p>
+          <p className={``}>
+            <img className="m-[10px]" src="https://img.shields.io/github/deployments/erikd256/stfranciscusheverlee/production?style=for-the-badge"/>
+            <img className="m-[10px]" src="https://img.shields.io/github/last-commit/erikd256/stfranciscusheverlee?style=for-the-badge"/>
+            <img className="m-[10px]" src="https://img.shields.io/github/license/erikd256/stfranciscusheverlee?style=for-the-badge"/>
+            <img className="m-[10px]" src="https://img.shields.io/scrutinizer/quality/g/erikd256/stfranciscusheverlee?style=for-the-badge"/>
+          </p>
           <span>
           <a className="mx-[0px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.facebook.com/pg/Franciscusgemeenschapheverlee"><FaFacebookF className={`${socialIconClasses}` }/></a>  
           <a className="mx-[4px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.youtube.com/channel/UC-7wzVLv2W3oUQtQcPpvCig"><FaYoutube className={`${socialIconClasses}` }/></a>
