565138f0f5c8eebe522be36429fbb923bac1e44d
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-08-04 14:51:00 +0200

    remove sitemap generator temporarily
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- next.config.js
+++ next.config.js
@@ -14,13 +14,3 @@ module.exports = withSvgr({
     ];
   },
 });
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap')
-    }
-
-    return config
-  },
-}
