42286d5425134f2650849063a04f7d00e8111ea6
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-08-05 22:02:41 +0200

    added sitemap.js
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

---
+++ pages/sitemap.js
@@ -0,0 +1,26 @@
+import { Layout } from "../components/layout";
+import {axios} from 'axios';
+import Head from 'next/head';
+
+export default function Sitemap() {
+  var XMLResult;
+  async function parseSitemap(){
+    axios.get("/sitemap.xml").then(
+      function (response) {
+        return (
+          <Layout>
+            <Head>
+              <title>Sitemap @ Sint-Franciscusparochie Heverlee</title>
+            </Head>
+            <div className="w-full bg-vijfdekleur text-basiskleur min-h-[20px] p-[25px]">
+              <span className="text-xl">Sitemap</span>
+              <br></br>
+              <button onClick={parseSitemap}>Parse Sitemap</button>
+              <span></span>
+            </div>
+          </Layout>
+        );
+      }
+    );
+   }
+}
