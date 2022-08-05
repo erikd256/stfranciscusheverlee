0d1cf5ae25f2af3530ede09b7a77cca73840b84c
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-08-05 20:00:34 +0200

    update sitemap.js
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -1,19 +1,13 @@
 import { Layout } from "../components/layout";
-const xml2js = require('xml2js');
-const axios = require('axios');
+import {axios} from 'axios';
 import Head from 'next/head';
-var parseString = require('xml2js').parseString;
+
 
 export default function Sitemap() {
   var XMLResult;
   async function parseSitemap(){
     axios.get("/sitemap.xml").then(
       function (response) {
-        parseString(response.data, function (err, result) {
-          console.log(result.urlset.url);
-          const XMLResult = result.urlset.url;
-          return XMLResult;
-        });
       }
     );
    }
