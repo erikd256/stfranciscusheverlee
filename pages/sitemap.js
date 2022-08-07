af82b5743f70a82c1098c3405dc9a9824a8e8293
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-08-07 11:41:11 +0200

    update sitemap.js
    Signed-off-by: erikd256 <62072464+erikd256@users.noreply.github.com>

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -3,13 +3,11 @@ import { Layout } from "../components/layout";
 export default function SiteMap() {
   return (
   <>
-    <title>Error 404</title>
-    <div className="text-center">
-      <img src="/olifant.svg" className="w-full"/>
-      <p className="text-xl">Ouch, we zijn in de diepe en grote jungle verdwaald.</p>
-      <a href="/" className="border-[2px] my-[20px] font-sans border-black p-2">Ga terug!</a>
-      <p className="text-lg">Deze pagina kon niet gevonden worden</p>
-    </div>
+    <Layout>
+      <Head>
+        <title>Sitemap @ Sint-Franciscusparochie Heverlee</title>
+      </Head>
+    </Layout>
   </>
   );
 }
