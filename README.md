6bd2ad1ede731d4112b1902d0b1b9ad993f17b0f
Erik D <62072464+erikd256@users.noreply.github.com>
2022-08-15 11:13:45 +0200

    update about page

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 Dit is de vernieuwde website van de Sint Franciscusparochie te Heverlee.
 
 Deze broncode is gebaseerd op de [TinaCMS Cloud Starter](https://github.com/tinacms/tina-cloud-starter) en zo hebben we besloten om ook onze website te licenseren onder de Apache 2.0 licensie. 
+
---
+++ pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import Head from 'next/head';
+import { Layout } from "../components/layout";
+import axios from 'axios';
+import gfm from 'remark-gfm';
+
+export default function About(){
+  const [readmedata, setReadmeData] = React.useState([]);
+  React.useEffect(() => {
+    axios.get('https://raw.githubusercontent.com/erikd256/stfranciscusheverlee/main/README.md')
+    .then(res => setReadmeData(res));
+  }, [])
+
+  return(
+    <Layout>
+      <Head>Over @ Sint-Franciscusparochie Heverlee</Head>
+      <ReactMarkdown className="w-[4/6]" remarkPlugins={[gfm]}>{readmedata.data}</ReactMarkdown>
+    </Layout>
+  )
+}
