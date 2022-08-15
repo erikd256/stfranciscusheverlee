6f614981d918084c5ec2590f9661c5ec4951acaf
Erik D <62072464+erikd256@users.noreply.github.com>
2022-08-15 21:12:34 +0200

    update sitemap.js

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -3,9 +3,11 @@ import { Layout } from "../components/layout";
 import GetSitemapLinks from "get-sitemap-links";
 
 export default function Sitemap() {
-  const SitemapLinks = GetSitemapLinks(
-    "https://www.stfranciscus-heverlee.org/sitemap.xml"
-  );
+  (async () => {
+    const array = await GetSitemapLinks(
+      "https://www.stfranciscus-heverlee.org/sitemap.xml"
+    );
+  })();
    return (
     <Layout>
       <Head>
@@ -14,7 +16,6 @@ export default function Sitemap() {
       <div className="w-3/4 left-[12.5%] relative bg-vijfdekleur text-basiskleur min-h-[20px] p-[25px]">
         <span className="text-xl">Sitemap</span>
         <br></br>
-        <span>{SitemapLinks.length}</span>
       </div>
     </Layout>
   );
