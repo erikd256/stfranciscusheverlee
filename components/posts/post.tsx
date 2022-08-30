2bbe26ddf8bad775863b850de16974638b5e4588
Erik D <62072464+erikd256@users.noreply.github.com>
2022-08-30 18:10:44 +0300

    update post.tsx

--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -19,87 +19,7 @@ import format from "date-fns/format";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import type { TinaMarkdownContent, Components } from "tinacms/dist/rich-text";
 import Head from 'next/head';
-
-declare global {
-  interface Window {
-    REMARK42: any;
-    remark_config: any;
-  }
-}
-
-// This function will insert the usual <script> tag of
-// Remark42 into the specified DOM location (parentElement)
-const insertScript = (id: string, parentElement: HTMLElement) => {
-  const script = window.document.createElement('script');
-  script.type = 'text/javascript';
-  script.async = true;
-  script.id = id;
-  /* For Gatsby it's important to manually provide the URL
-  and make sure it does not contain a trailing slash ("/").
-  Because otherwise the comments for paths with/without 
-  the trailing slash are stored separately in the BoltDB database.
-  When following a Gatsby Link a page is loaded without the trailing slash,
-  but when refreshing the page (F5) it is loaded with the trailing slash.
-  So essentially every URL can become duplicated in the DB and you may not see
-  your comments from the inverse URL at your present URL.
-  Making sure url is provided without the trailing slash
-  in the remark42 config solves this. */
-  let url = window.location.origin + window.location.pathname;
-  if (url.endsWith('/')) {
-    url = url.slice(0, -1);
-  }
-  // Now the actual config and script-fetching function:
-  script.innerHTML = `
-    var remark_config = {
-      host: "https://remark42.stfranciscus-heverlee.org",
-      site_id: "remark42.stfranciscus-heverlee.org",
-    };
-    !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`;
-  parentElement.appendChild(script);
-};
-
-/* This function removes the previously added script from the DOM.
-Might be necessary when page transitions happen, to make sure a new instance 
-is created, pointing to the current URL. Although this might actually 
-not be necessary, please do your own testing. */
-const removeScript = (id: string, parentElement: HTMLElement) => {
-  const script = window.document.getElementById(id);
-  if (script) {
-    parentElement.removeChild(script);
-  }
-};
-
-// This function will be provided to useEffect and will insert the script
-// when the component is mounted and remove it when it unmounts
-const manageScript = () => {
-  if (!window) {
-    return () => {};
-  }
-  const { document } = window;
-  if (document.getElementById('remark42')) {
-    insertScript('comments-script', document.body);
-  }
-  return () => removeScript('comments-script', document.body);
-};
-
-/* Another function for another useEffect - this is the most crucial part for
-Gatsby compatibility. It will ensure that each page loads its own appropriate
-comments, and that comments will be properly loaded */
-const recreateRemark42Instance = () => {
-  if (!window) {
-    return;
-  }
-  const remark42 = window.REMARK42;
-  if (remark42) {
-    remark42.destroy();
-    remark42.createInstance(window.remark_config);
-  }
-};
-
-type CommentsProps = {
-  location: string;
-};
-
+import Script from 'next/script'
 
 const components: Components<{
   BlockQuote: {
@@ -224,7 +144,18 @@ export const Post = (props) => {
           <TinaMarkdown components={components} content={props._body} />
         </div>
       </Container>
-      <div id="remark42" />
+      <div id="remark42"></div>
+      <Script
+  dangerouslySetInnerHTML={{
+    __html: `
+    var remark_config = {
+      host: 'https://remark42.stfranciscus-heverlee.org',
+      site_id: 'stfranciscus-heverlee.org',
+    };
+    window.remark_config = remark_config;
+          !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`,
+  }}
+></Script>
     </Section>
     </>
   );
