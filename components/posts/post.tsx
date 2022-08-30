88f35f81db508ce1d23f2263edadba717840ac4c
Erik D <62072464+erikd256@users.noreply.github.com>
2022-08-30 18:29:43 +0300

    add post.tsx

--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -89,18 +89,6 @@ export const Post = (props) => {
           <title>{`${props.title} @ Sint-Franciscusparochie Heverlee`}</title>
           <meta name="keywords" content={props.keywords}/>
           <meta name="description" content={props.desc}/>
-          <Script
-  dangerouslySetInnerHTML={{
-    __html: `
-    var remark_config = {
-      host: 'https://remark42.stfranciscus-heverlee.org',
-      site_id: 'stfranciscus-heverlee.org',
-      theme: 'dark',
-    };
-    window.remark_config = remark_config;
-          !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`,
-  }}
-></Script>
     </Head>
     <Section className="flex-1">
       <Container className={`flex-1 max-w-full pb-2`} size="large">
@@ -157,6 +145,17 @@ export const Post = (props) => {
         </div>
       </Container>
       <div id="remark42">{''}</div>
+          <Script
+  dangerouslySetInnerHTML={{
+    __html: `
+    var remark_config = {
+      host: 'https://remark42.stfranciscus-heverlee.org',
+      site_id: 'stfranciscus-heverlee.org',
+    };
+    window.remark_config = remark_config;
+          !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`,
+  }}
+></Script>
     </Section>
     </>
   );
