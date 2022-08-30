56aafce1165b6c68f63ffb43e89273770a1c8667
Erik D <62072464+erikd256@users.noreply.github.com>
2022-08-30 18:39:24 +0300

    added post.tsx

--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -150,6 +150,7 @@ export const Post = (props) => {
     const remark_config = {
       host: 'https://remark42.stfranciscus-heverlee.org',
       site_id: 'remark42.stfranciscus-heverlee.org',
+      show_rss_subscription:: 'true',
     };
     window.remark_config = remark_config;
           !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`,
