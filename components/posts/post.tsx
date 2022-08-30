08e98629cbe452c6c57fec210d05273025404a36
Erik D <62072464+erikd256@users.noreply.github.com>
2022-08-30 18:52:48 +0300

    update post.tsx

--- components/posts/post.tsx
+++ components/posts/post.tsx
@@ -154,7 +154,18 @@ export const Post = (props) => {
       show_rss_subscription: 'true',
     };
     window.remark_config = remark_config;
-          !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`,
+    !(function (e, n) {
+      for (var o = 0; o < e.length; o++) {
+      var r = n.createElement("script"),
+      c = ".js",
+      d = n.head || n.body;
+      "noModule" in r ? ((r.type = "module"), (c = ".mjs")) : (r.async = !0),
+      (r.defer = !0),
+      (r.src = remark_config.host + "/web/" + e[o] + c),
+      d.appendChild(r);
+      }
+      })(remark_config.components || ["embed"], document);
+      `,
   }}
 ></script>
     </Section>
