3573c2b39395df8446bd906b9c032125b36778ce
Erik D <62072464+erikd256@users.noreply.github.com>
2022-09-02 23:14:19 +0300

    update iframe layout

--- components/blocks/weeknieuws.tsx
+++ components/blocks/weeknieuws.tsx
@@ -21,7 +21,7 @@ export const Weeknieuws = ({ data, parentField = "" }) => {
         <>
           <div className="fixed w-3/4 h-3/4 z-[500] left-[12.5%] top-[12.5%] bg-vijfdekleur rounded-xl">
             <button onClick={handleClick} className="absolute text-basiskleur right-[10px] h-[20px] w-[20px] top-[10px]">&#x2715;</button>
-            <iframe src={data.file} className="w-full h-full rounded.xl"></iframe>
+            <iframe src={data.file} className="w-full h-full rounded-xl"></iframe>
           </div>
           <div className="opacity-90 fixed bg-basiskleur z-[499] left-[0px] w-full h-full top-[0px]"></div>
         </>
