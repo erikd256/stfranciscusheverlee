258fd9513e0e7b65658eb65e6a4220f76400ef65
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-10-29 19:12:49 +0300

    added matomo analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import "../styles.css";
 import TinaProvider from "../.tina/components/TinaDynamicProvider";
+import React, { useEffect } from "react";
+
+import { init } from "@socialgouv/matomo-next";
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
 
 const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }, []);
   return (
     <TinaProvider>
       <Component {...pageProps} />
