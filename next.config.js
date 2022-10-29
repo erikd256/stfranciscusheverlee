d98dc561b7ff213a75c3aa74f42dfeb3e678e706
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-10-29 19:58:15 +0300

    add matomo and remove sitemap as it is built by netlify

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withSvgr = require("next-svgr");
 module.exports = withSvgr({
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap')
-    }
+  webpack: (config) => {
     return config
   },
   async rewrites() {
