83442dac1d7bbf4e08bfe627ceae98ce0c02cb5d
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-10-30 13:52:25 +0200

    added page for form-submitted

---
+++ pages/form-submitted.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import { Layout } from "../components/layout";
+
+export default function Sitemap() {
+   return (
+    <Layout className="overflow-x-hidden">
+      <Head>
+        <meta name="robots" content="noindex"/>
+        <title>Formulier verzonden!</title>
+      </Head>
+      <span className='text-2xl relative mt-[40px] w-1/2 left-1/4'>Formulier verzonden!</span>
+      <br></br>
+      <span className='text-lg relative left-1/4 w-3/4 mb-[100px]'>We proberen je antwoorden zo snel mogelijk te verwerken.</span>
+    </Layout>
+  );
+}
