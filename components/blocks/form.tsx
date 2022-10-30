48cda4c345ad4c8dcdb71cd2b21fa9e7e7b0df15
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-10-30 18:39:15 +0200

    update recaptcha theme

--- components/blocks/form.tsx
+++ components/blocks/form.tsx
@@ -33,7 +33,7 @@ export const Form = ({ data, parentField }) => {
             );
           })}
           <input type="hidden" name="form-name" value={data.NetlifyId} />
-          <ReCAPTCHA sitekey="6LfKUsYiAAAAAE3CN8lLY6Kv1uPGzcdTUZbzYurz"/>
+          <ReCAPTCHA sitekey="6LfKUsYiAAAAAE3CN8lLY6Kv1uPGzcdTUZbzYurz" theme="dark"/>
           <p className="inline-flex"><label>Ik ga akkoord met de privacyovereenkomst.</label><input required type="checkbox" className="mx-[10px]"/><span className="text-red-500">*</span></p>          
           <button type="submit" title="Versturen" placeholder="Versturen" className="p-[4px] my-[15px] rounded-[5px] border-[2px] border-basiskleur bg-liturgischekleur w-full">Versturen</button>
           <span className="text-red-500 text-sm">* Verplicht veld</span>
