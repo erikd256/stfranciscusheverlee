4bc05edb231893e504b061d71b9c28eaeb163053
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-10-30 16:11:37 +0200

    update form.tsy

--- components/blocks/form.tsx
+++ components/blocks/form.tsx
@@ -32,7 +32,8 @@ export const Form = ({ data, parentField }) => {
               />
             );
           })}
-          <ReCAPTCHA sitekey="6LfKUsYiAAAAAE3CN8lLY6Kv1uPGzcdTUZbzYurz" />          
+          <ReCAPTCHA sitekey="6LfKUsYiAAAAAE3CN8lLY6Kv1uPGzcdTUZbzYurz" />
+          <p className="inline-flex"><label>Ik ga akkoord met de privacyovereenkomst.</label><input required type="checkbox" className="mx-[10px]"/><span className="text-red-500">*</span></p>          
           <button type="submit" title="Versturen" placeholder="Versturen" className="p-[4px] my-[15px] rounded-[5px] border-[2px] border-basiskleur bg-liturgischekleur w-full">Versturen</button>
           <span className="text-red-500 text-sm">* Verplicht veld</span>
           </form>
