027a7285684238228939f6a3dffc08ee572e2a0a
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-10-30 15:47:43 +0200

    update recaptcha

--- components/blocks/form.tsx
+++ components/blocks/form.tsx
@@ -4,8 +4,6 @@ import { Container } from "../util/container";
 import { Icon } from "../util/icon";
 import type { TinaTemplate } from "tinacms";
 import { iconSchema } from "../util/icon";
-import Head from "next/head";
-import ReCAPTCHA from "react-google-recaptcha";
 
 export const Question = ({ data, tinaField }) => {
   return (
@@ -25,7 +23,7 @@ export const Form = ({ data, parentField }) => {
         size="large"
       >
         <p className="text-2xl my-[10px]">{data.airformTitle}</p>
-        <form name={data.NetlifyId} method="POST" action="/form-submitted" data-netlify="true" className="text-md">
+        <form name={data.NetlifyId} method="POST" data-netlify-recaptcha="true" action="/form-submitted" data-netlify="true" className="text-md">
         {data.items &&
           data.items.map(function (block, i) {
             return (
@@ -36,7 +34,6 @@ export const Form = ({ data, parentField }) => {
               />
             );
           })}
-          <ReCAPTCHA sitekey="6LfKUsYiAAAAAE3CN8lLY6Kv1uPGzcdTUZbzYurz" />          
           <button type="submit" title="Versturen" placeholder="Versturen" className="p-[4px] my-[15px] rounded-[5px] border-[2px] border-basiskleur bg-liturgischekleur w-full">Versturen</button>
           <span className="text-red-500">* Verplicht veld</span>
           </form>
