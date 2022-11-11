5e40566850e215d4a9e8a3fdd955445709005694
erikd256 <62072464+erikd256@users.noreply.github.com>
2022-11-11 23:04:56 +0200

    added vercel badge

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -7,10 +7,10 @@ export const Footer = ({ data, icon, rawData}) => {
   return (
     <>
     <footer className={`bg-basiskleur pb-[75px] min-h-[500px] relative pt-[20px]`}>
-        <div className="grid w-full text-liturgischekleur gap-6 grid-cols-1 place-items-center">
+        <div className="grid w-full text-liturgischekleur gap-6 grid-cols-1 place-items-center pt-[20px] mo:pt-[80px]">
           <img src="/icon.svg" width="50px"/>
           <p>&copy; 2022 - {(new Date().getFullYear())} <a href="https://github.com/erikd256" target="_blank">erikd256</a></p>
-          <div className="w-full mo:w-1/2 mo: text-center place-content-center mo:absolute mo:top-[100px] mo:right-[0px]">
+          <div className="w-full mo:w-1/3 mo: text-center place-content-center mo:absolute mo:top-[100px] mo:right-[0px]">
           
             <a className="mx-[0px] mt-[30px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.facebook.com/Franciscusgemeenschapheverlee"><FaFacebookF className={`${socialIconClasses}` }/></a>  
             <a className="mx-[4px] inline-block opacity-80 hover:opacity-100 transition ease-out duration-150" target="_blank" href="https://www.youtube.com/channel/UC-7wzVLv2W3oUQtQcPpvCig"><FaYoutube className={`${socialIconClasses}` }/></a>
@@ -30,10 +30,11 @@ export const Footer = ({ data, icon, rawData}) => {
             <br></br>
             <a href="https://status.stfranciscus-heverlee.org/" className="no-underline">Status</a>
             <br></br>
+            <img src="https://vercelbadge.vercel.app/api/erikd256/stfranciscusheverlee" className="inline"/>
             </span>
           </div>
         </div>
-        <div className="grid mo:left-[0px] mo:absolute mo:top-[100px] mo:w-1/2 w-full mo:mt-[0px] mt-[50px] text-sm text-liturgischekleur grid-cols-1 place-items-left text-center">
+        <div className="grid mo:left-[0px] mo:absolute mo:top-[100px] mo:w-1/3 w-full mo:mt-[0px] mt-[50px] text-sm text-liturgischekleur grid-cols-1 place-items-left text-center">
         <span className="text-md">Sint Franciscusparochie</span><br></br>
         <span className="text-xs">Heverlee - Kessel-lo - Leuven<br></br>Federatie Kesselinde<br></br></span>
         <hr className="border-liturgischekleur my-[10px]"></hr>
