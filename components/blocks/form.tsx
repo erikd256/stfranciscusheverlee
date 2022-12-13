f5a942669bb060a3308e609551e5c9aaf9639b1d
erikd256 <erikd256@duck.com>
2022-12-13 22:23:45 +0200

    make recaptcha required

--- components/blocks/form.tsx
+++ components/blocks/form.tsx
@@ -34,7 +34,7 @@ export const Form = ({ data, parentField }) => {
             );
           })}
           <p className="inline-flex"><label>Ik ga akkoord met de privacyovereenkomst.</label><input required type="checkbox" className="mx-[10px]"/><span className="text-red-500">*</span></p>          
-          <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}/>
+          <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY} required="true"/>
           <button  className={`my-[10px] rounded-[5px] border-basiskleur bg-liturgischekleur border-[2px] w-full`} type="submit">Versturen</button>
           <span className="text-red-500 text-sm">* Verplicht veld</span>
           </form>
