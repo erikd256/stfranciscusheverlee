4a659298112ec01f689d248b39c55792f22b62a5
erikd256 <erikd256@duck.com>
2022-12-31 17:28:51 +0200

    use repo based media

--- .tina/config.ts
+++ .tina/config.ts
@@ -11,13 +11,17 @@ import { videoBlockSchema } from "../components/blocks/video";
 import { headingBlockSchema } from "../components/blocks/heading";
 import { audioBlockSchema } from "../components/blocks/audio";
 import { weeknieuwsBlockSchema } from "../components/blocks/weeknieuws";
-import { WebDAVMediaStore } from 'tinacms-webdav-mediastore'
 
 const config = defineStaticConfig({
   branch: "main",
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
   token: process.env.TINATOKEN!,
-  media: new WebDAVMediaStore(),
+  media: {
+    tina: {
+      publicFolder: 'public',
+      mediaRoot: 'uploads',
+    },
+  },
 build: {
   publicFolder: "public", // The public asset folder for your framework
   outputFolder: "admin", // within the public folder
