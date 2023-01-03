2d0651faeb71e034a2dd4c186aefdf1a6502f3d1
erikd256 <erikd256@duck.com>
2023-01-03 22:41:59 +0200

    use custom mediastore

--- .tina/config.ts
+++ .tina/config.ts
@@ -11,17 +11,12 @@ import { videoBlockSchema } from "../components/blocks/video";
 import { headingBlockSchema } from "../components/blocks/heading";
 import { audioBlockSchema } from "../components/blocks/audio";
 import { weeknieuwsBlockSchema } from "../components/blocks/weeknieuws";
-
+import { NextCloudMediaStore } from "tinacms-nextcloud-mediastore";
 const config = defineStaticConfig({
   branch: "main",
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
   token: process.env.TINATOKEN!,
-  media: {
-    tina: {
-      publicFolder: 'public',
-      mediaRoot: 'uploads',
-    },
-  },
+  media: new NextCloudMediaStore(),
 build: {
   publicFolder: "public", // The public asset folder for your framework
   outputFolder: "admin", // within the public folder
