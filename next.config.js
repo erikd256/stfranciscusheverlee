a6a4e118419bcb8276cdb6d0adab8ddf21f07121
Erik D. (@erikd256) <62072464+erikd256@users.noreply.github.com>
2023-04-09 19:11:16 +0300

    update security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 const withSvgr = require("next-svgr");
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' *.stfranciscus-heverlee.org;
+  child-src none;
+  style-src 'self';
+  font-src 'self';  
+`
+
 module.exports = withSvgr({
   webpack: (config, { isServer }) => {
     if (isServer) {
@@ -18,6 +26,18 @@ module.exports = withSvgr({
         destination: '/admin/index.html',
       },
     ];
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [{
+          key: 'Content-Security-Policy',
+          value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+        }],
+      },
+    ]
   },
   i18n: {
     locales: ['nl'],
