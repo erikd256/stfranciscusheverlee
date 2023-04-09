9980178f03d195efd6a672af3488fdd22e8e1565
Erik D. (@erikd256) <62072464+erikd256@users.noreply.github.com>
2023-04-09 22:45:34 +0300

    update headers

--- next.config.js
+++ next.config.js
@@ -22,15 +22,37 @@ module.exports = withSvgr({
   },
   async headers() {
     return [
-      {
-        // Apply these headers to all routes in your application.
-        source: '/:path*',
-        headers: [{
-          key: 'Content-Security-Policy',
-          value:" default-src 'self' https://*.stfranciscus-heverlee.org https://assets.tina.io https://vitals.vercel-insights.com https://identity.tinajs.io https://fonts.googleapis.com/; script-src 'nonce-plausibleanalytics' *.stfranciscus-heverlee.org 'unsafe-inline'; child-src 'none'; style-src 'self' *.stfranciscus-heverlee.org; font-src 'self'; object-src 'none'; base-uri 'none'; require-trusted-types-for 'script';"
-      }],
-      },
-    ]
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Content-Security-Policy',
+              value:
+                "default-src 'self' https://*.stfranciscus-heverlee.org https://assets.tina.io https://vitals.vercel-insights.com https://identity.tinajs.io https://fonts.googleapis.com/; script-src 'nonce-plausibleanalytics' 'unsafe-inline'; child-src 'none'; style-src 'self'; font-src 'self'; object-src 'none'; base-uri 'none'; require-trusted-types-for 'script';",
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'origin-when-cross-origin',
+            },
+            {
+              key: 'Strict-Transport-Security',
+              value: 'max-age=3571000; includeSubDomains; preload',
+            }
+          ],
+        },
+      ];
   },
   i18n: {
     locales: ['nl'],
