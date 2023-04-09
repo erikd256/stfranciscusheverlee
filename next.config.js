c3c3ca0358d6c91efc787093bb7cb468b4e7b0ac
Erik D. (@erikd256) <62072464+erikd256@users.noreply.github.com>
2023-04-09 22:47:24 +0300

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -36,7 +36,7 @@ module.exports = withSvgr({
             {
               key: 'Content-Security-Policy',
               value:
-                "default-src 'self' https://*.stfranciscus-heverlee.org https://assets.tina.io https://vitals.vercel-insights.com https://identity.tinajs.io https://fonts.googleapis.com/; script-src 'nonce-plausibleanalytics' 'unsafe-inline'; child-src 'none'; style-src 'self'; font-src 'self'; object-src 'none'; base-uri 'none'; require-trusted-types-for 'script';",
+                "default-src 'self' https://*.stfranciscus-heverlee.org https://assets.tina.io https://vitals.vercel-insights.com https://identity.tinajs.io https://fonts.googleapis.com/; script-src 'nonce-plausibleanalytics'; child-src 'none'; style-src 'self'; font-src 'self'; object-src 'none'; base-uri 'none'; require-trusted-types-for 'script';",
             },
             {
               key: 'X-Content-Type-Options',
