5323dda312473afe0c9466b386f717a1b0342eeb
Erik D. <62072464+erikd256@users.noreply.github.com>
2023-09-18 18:51:28 +0200

    add matomo analytics

--- components/blocks/weeknieuws.tsx
+++ components/blocks/weeknieuws.tsx
@@ -3,27 +3,11 @@ import React from "react";
 import {useState} from 'react';
 
 export const Weeknieuws = ({ data, parentField = "" }) => {
-  const [isShown, setIsShown] = useState(false);  
-  const handleClick = event => {
-    setIsShown(current => !current);
-  };
-  return (
-    <>
-    <div className="max-w-7xl mx-auto px-6 sm:px-8">
-      <button onClick={handleClick} className="underline">{data.LinkTitle}</button>
-      {isShown && (
-        <>
-          <div className="fixed w-full z-[5000] h-[calc(100%-70px)] p-0 top-[70px] left-0 bg-vijfdekleur">
-            <button onClick={handleClick} className="absolute text-vierdekleur right-[10px] h-[20px] w-[20px] top-[40px]">&#x2715;</button>
-            <iframe src={data.file} className="w-full h-full m-0"/> 
-          </div>
-        </>
-      )}
-      </div>
-      </>
-  );
-};
 
+    return (
+      <>Eventjes geduld. hier is nog niets te zien</>
+    );
+  }
 
 export const weeknieuwsBlockSchema: TinaTemplate = {
   name: "weeknieuws",
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles.css";
 import React, { useEffect } from "react";
 
-const App = ({ Component, pageProps }) => {
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
+
+const App = ({ Component, pageProps }) => {useEffect(() => {
+  init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+}, []);
   return (
       <Component {...pageProps} />
   );
