fcc046b90b8beccfb4498fcc8e84f2bbb83a9a01
Erik D. <62072464+erikd256@users.noreply.github.com>
2023-09-28 19:02:48 +0300

    added vercel headers

--- components/weeknieuws/parochiebladeren.tsx
+++ components/weeknieuws/parochiebladeren.tsx
@@ -7,20 +7,25 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from 'moment'
 import formatISO from "date-fns/formatISO"
 export const Parochiebladeren = ({ data }) => {
-
+  const [filesrc, setFilesrc] = React.useState("");
+  const [iframeHidden, setIframeHidden] = React.useState(true);
   const localizer = momentLocalizer(moment) // or globalizeLocalizer
   const eventsList = [];
   data.map((postData) => {
     const post = postData.node;
     const Event = {
-      title: "Week: " + moment().week(post.date),
-      start: moment.utc(post.date),
-      end: moment.utc(post.date),
+      title: "Week: " + moment(moment.utc(post._values.date)).week(),
+      start: moment.utc(post._values.date),
+      end: moment.utc(post._values.date),
       allDay: true,
-      resource: post.bestand,
+      resource: post._values.bestand,
     }
     eventsList.push(Event)
   })
+  const handleSelectEvent = React.useCallback(
+    (event) => setFilesrc(event.resource)
+    , []
+  )
   return (
     <>
     <div className="h-[600px]">
@@ -30,8 +35,11 @@ export const Parochiebladeren = ({ data }) => {
     showMultiDayTimes
     step={60}
     events={eventsList}
+    onSelectEvent={handleSelectEvent}
   /></div>
-      
+      <div className="">
+        <iframe className={`h-full w-full bg-basiskleur z-[90] top-0 left-0 border-0 z-[9000] ${iframeHidden ? "none":"block" }`} src={`https://viewer.stfranciscus-heverlee.org/web/viewer.html?file=https://stfranciscus-heverlee.org/${filesrc}`}/>
+      </div>
       <Head>
         <title>Nieuws @ Sint-Franciscusparochie Heverlee</title>
         <meta name="keywords" content="nieuws,parochienieuws"/>
