6a9bce7eb4a529ca3f5b6e33ca0eb30de9568de4
Erik D. <62072464+erikd256@users.noreply.github.com>
2023-10-01 11:05:10 +0300

    add recaptcha

--- components/blocks/form.tsx
+++ components/blocks/form.tsx
@@ -15,10 +15,6 @@ export const Question = ({ data, tinaField }) => {
 };
 
 export const Form = ({ data, parentField }) => {
-  const [captcha, setCaptcha] = React.useState(false);
-  function setSolved(){
-    setCaptcha(!captcha);
-  }
   return (
     <Section>
       <Container
@@ -38,9 +34,7 @@ export const Form = ({ data, parentField }) => {
             );
           })}
           <p className="inline-flex"><label>Ik ga akkoord met de privacyovereenkomst.</label><input required type="checkbox" className="mx-[10px]"/><span className="text-red-500">*</span></p>          
-          <HCaptcha sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY} onVerify={token => setSolved()}/>
-          <span className="text-red-500 text-sm font-bold" style={{display: !captcha ? "inline-block":"none"}}>Ben je toch een robot? Klik op het vak hierboven...</span>
-          <button  className={`my-[10px] rounded-[5px] border-basiskleur bg-liturgischekleur border-[2px] w-full disabled:cursor-not-allowed`} type="submit" disabled={!captcha}>Versturen</button>
+          <button  className={`my-[10px] rounded-[5px] border-basiskleur bg-liturgischekleur border-[2px] w-full disabled:cursor-not-allowed`} type="submit">Versturen</button>
           <span className="text-red-500 text-sm">* Verplicht veld</span>
           <input hidden type={"text"} name="sendToEmail" value={data.NetlifyId} />
           </form>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles.css";
 import React, { useEffect } from "react";
-
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import { init } from "@socialgouv/matomo-next";
 
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL;
@@ -10,7 +10,17 @@ const App = ({ Component, pageProps }) => {useEffect(() => {
   init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
 }, []);
   return (
+    <GoogleReCaptchaProvider
+      reCaptchaKey={process.env.RECAPTCHA_SITE_KEY}
+      scriptProps={{
+        async: false,
+        defer: false,
+        appendTo: "head",
+        nonce: undefined,
+      }}
+      >
       <Component {...pageProps} />
+    </GoogleReCaptchaProvider>
   );
 };
 
