bbc594f4c8b556134dd8a5371c4ba4c916e17534
Erik D. <62072464+erikd256@users.noreply.github.com>
2023-10-24 20:48:03 +0300

    update gallery component

--- components/blocks/gallery.tsx
+++ components/blocks/gallery.tsx
@@ -12,7 +12,7 @@ export const Gallery = ({ data, parentField = "" }) => {
   const [galleryHidden, setGalleryHidden] = React.useState(true)
   const [titleHidden, setTitleHidden] = React.useState(true)
   const [imageSrc, setImageSrc] = React.useState("")
-  const [imageNumber, setImageNumber] = React.useState("1")
+  const [imageNumber, setImageNumber] = React.useState(0)
 
   function getGallery(){
     axios({
@@ -38,6 +38,20 @@ export const Gallery = ({ data, parentField = "" }) => {
   function toggleTitle(){
     setTitleHidden(!titleHidden)
   }
+  function increaseImageNumber(){
+    if(imageNumber < (srcArray.length-1)){
+      setImageNumber(imageNumber+1)
+    }else{
+      setImageNumber(0)
+    }
+  }
+  function decreaseImageNumber(){
+    if(imageNumber > 0){
+      setImageNumber(imageNumber-1)
+    }else{
+      setImageNumber((srcArray.length-1))
+    }
+  }
   return (
     <>
       <div className="p-[50px] container w-1/4 h-[260px] relative m-[50px]" onMouseEnter={toggleTitle} onMouseLeave={toggleTitle}>
@@ -46,9 +60,11 @@ export const Gallery = ({ data, parentField = "" }) => {
       </div>
       <div className={`fixed p-0 z-[5000] h-[50%] w-[50%] bg-basiskleur top-[10%] left-[25%] border-2 border-derdekleur rounded-md object-contain ${galleryHidden ? "hidden":"block" }`}>
         <span className="absolute top-[10px] text-lg w-full text-center text-liturgischekleur"><b>{galleryTitle}</b></span>
-        <img className="mx-auto self-center relative top-[50px] max-w-full rounded-xl object-contain" src={srcArray[0]}></img>
-        <span className="absolute bottom-[10px] w-full text-sm text-center text-liturgischekleur">{imageNumber}/{srcArray.length}</span>
+        <img className="mx-auto self-center relative top-[50px] max-w-full rounded-xl object-contain" src={srcArray[imageNumber]}></img>
+        <span className="absolute bottom-[10px] w-full text-sm text-center text-liturgischekleur"><button onClick={decreaseImageNumber} className="text-4xl mr-4">&#9756;</button>
+        <span>{imageNumber+1}/{srcArray.length}</span><button className="pt-4 text-4xl ml-4" onClick={increaseImageNumber}>&#9758;</button></span>
         <button className={`fixed rounded-md border-1 border-basiskleur p-[5px] top-[13%] left-[27%] bg-liturgischekleur ${galleryHidden ? "hidden":"block" } z-[9999]`} onClick={hideGallery}>Sluiten</button>
+        
       </div>
     </>
   );
