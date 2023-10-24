2fc758078804748ae1d0bbc49b30340aa42723f2
Erik D. <62072464+erikd256@users.noreply.github.com>
2023-10-24 16:58:13 +0300

    add api for fetching google photo albums
    Signed-off-by: Erik D. <62072464+erikd256@users.noreply.github.com>

---
+++ pages/api/googlephotos.ts
@@ -0,0 +1,30 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+const { Curl } = require('node-libcurl');
+const curl = new Curl();
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>
+  ) {
+      curl.setOpt('URL', req.body.photoAlbumID || "");
+      curl.setOpt('FOLLOWLOCATION', true);
+      const regex = /https:\/\/lh3\.googleusercontent\.com\/pw\/[a-zA-Z0-9\-_]*/gm // the only difference is the [ at the beginning
+      const regexTitle = /<title>[a-zA-Z0-9\-_ ]*/gm
+      curl.on('end', function (statusCode, data, headers) {
+        console.info(statusCode);
+        console.info('---');
+        console.info(data.length);
+        console.info('---');
+        console.info(this.getInfo( 'TOTAL_TIME'));
+        const links = new Set(data.match(regex))
+        const titleString1 = data.match(regexTitle).toString().replaceAll("<title>", "")
+        const titleString2 = titleString1.replaceAll(" - Google Photos","")
+        res.json({images: Array.from(links), title: titleString2, coverImage: links[0]})
+        this.close()
+      });
+
+      curl.on('error', curl.close.bind(curl));
+      curl.perform();
+  }
+
+
