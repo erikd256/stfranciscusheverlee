2edfe1ed64b82b04422104e2bd9c77d4a447d95e
Erik D. @erikd256 <62072464+erikd256@users.noreply.github.com>
2024-02-11 23:10:18 +0200

    remove csp

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 const withSvgr = require("next-svgr");
 
-const cspHeader = `
-default-src 'self' "https://*.tina.io";
-script-src 'self' 'unsafe-inline' 'https://www.google.com' 'https://www.gstatic.com' 'https://www.youtube.com' 'https://www.stfranciscus-heverlee.org';
-style-src 'self' 'unsafe-inline';
-object-src 'https://opencollective.com';
-base-uri 'self';
-connect-src 'https://content.tinajs.io' 'https://identity.tinajs.io';
-font-src 'self' 'unsafe-inline' 'https://www.stfranciscus-heverlee.org' data:;
-frame-src 'self' 'https://www.google.com' 'https://www.youtube.com';
-img-src 'self' 'https://lh3.googleusercontent.com' 'https://opencollective.com' 'https://img.shields.io' 'https://assets.tina.io';
-manifest-src 'self';
-media-src 'self';
-worker-src 'none';
-`
- 
-
 module.exports = withSvgr({
   webpack: (config, { isServer }) => {
     return config
@@ -65,10 +49,6 @@ module.exports = withSvgr({
               key: 'Strict-Transport-Security',
               value: 'max-age=3571000; includeSubDomains; preload',
             },
-            {
-              key: 'Content-Security-Policy',
-              value: cspHeader.replace(/\n/g, ''),
-            }
           ],
         },
       ];
