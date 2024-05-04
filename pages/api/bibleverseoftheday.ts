74327172cdf858c3f24e9b6b0270afee82a587ae
Erik D <62072464+erikd256@users.noreply.github.com>
2024-05-04 16:30:20 +0300

    update api page to not bloat console

--- pages/api/bibleverseoftheday.ts
+++ pages/api/bibleverseoftheday.ts
@@ -5,7 +5,6 @@ export default async (req, res) => {
   const bibleversesplitted1 = bibleversescript.data.split("\\u003cdiv");
   const bibleverse = bibleversesplitted1[1].split("\\u003e")[1].replace("\\u003c/div", "");
   const author = bibleversesplitted1[2].split("target=\\")[1].replace(`"_blank\\"\\u003e`, "").split("\\u003c")[0]
-  console.log()
   const bibleData = {
     verse: bibleverse,
     slug: author
