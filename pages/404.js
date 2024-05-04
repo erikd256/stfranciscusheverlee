3888d1d6c3d3e7ef97272db124cd81b100c04105
Erik D <62072464+erikd256@users.noreply.github.com>
2024-05-04 17:29:30 +0300

    update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 export default function FourOhFour() {
-  const windowSize = React.useRef([
-    window.innerWidth,
-    window.innerHeight,
-  ]);
+  const [windowWidth,setWindowWidth] = React.useState(""); 
+  const [windowHeight,setWindowHeight] = React.useState(""); 
+
+  React.useEffect({
+    if(window){
+      setWindowWidth(window.innerWidth);
+      setWindowHeight(window.innerHeight);
+    }
+  });
   return (
   <>
     <title>Error 404</title>
-    <img src={`https://picsum.photos/${windowSize.current[0]}/${windowSize.current[1]}`} className="z-[-1] fixed w-full h-full"/>
+    <img src={`https://picsum.photos/${windowWidth}/${windowHeight}`} className="z-[-1] fixed w-full h-full"/>
     <div className="fixed w-1/2 h-1/2 border-2 border-white bg-black top-1/4 left-1/4 text-white text-center py-[20px] opacity-70 leading-[50px]">
      
       <p className="text-4xl">Deze Pagina is helaas niet meer te vinden.</p>
