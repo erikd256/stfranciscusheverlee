db9299fba06dcaf96a6b7885f71af0b9357936b3
Erik D. @erikd256 <erikd256@users.noreply.github.com>
2024-08-25 21:04:12 +0300

    update search index

--- pages/api/search/index.json.js
+++ pages/api/search/index.json.js
@@ -1,9 +1,19 @@
+import { client } from '../../../tina/__generated__/client'
+
+const postsResponse = await client.queries.postConnection()
+const pagesResponse = await client.queries.pageConnection()
+
+const posts = postsResponse.data.postConnection.edges.map((post) => {
+  return { slug: post.node._sys.filename, description: post.node.desc }
+})
+const pages = pagesResponse.data.pageConnection.edges.map((post) => {
+  return { slug: post.node._sys.filename, description: post.node.label}
+})
+
+const index = posts.concat(pages);
 
 export default async (req, res) => {
-  res.writeHead(404, {
-    "Content-Type": "text/xml"
-  });
     res.status(200)
-    res.end("xml")
+    res.end(JSON.stringify(index))
   }
     
